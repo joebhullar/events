ade7ae998ad4973c6a43748ff226f744ac2d3211
joebhullar <joebhullar11@gmail.com>
2018-04-21 18:25:40 +0000

    second commit

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ const con = mysql.createConnection({
 });
 
 const siteTitle = "Simple Application";
-const baseURL = "http://localhost:80/"
+const baseURL = "http://206.189.64.55:80/";
 
 app.get('/', function (req, res) {
-  con.query("SELECT * FROM track ORDER BY id DESC", function (err, result) {
+  con.query("SELECT * FROM tracking ORDER BY id DESC", function (err, result) {
 	console.log(result);
 	res.render('pages/index',{
 		siteTitle: siteTitle,
@@ -38,6 +38,69 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/event/add', function (req, res) {
+  con.query("SELECT * FROM tracking ORDER BY id DESC", function (err, result) {
+        console.log(result);
+        res.render('pages/add-event.ejs',{
+                siteTitle: siteTitle,
+                pageTitle: "Add new event",
+                items : ''
+                });
+        });
+
+});
+
+app.post('/event/add', function(req,res){
+	var query = "INSERT INTO `tracking` (comments, rating) VALUES (";
+//	    query +=  " '"+req.body.starttime+"',";
+            query +=  " '"+dateFormat(req.body.starttime,"yyyy-mm-dd'T'HH:MM:ss")+"',";
+	    query +=  " '"+req.body.comments+"',";
+	    query +=  " '"+req.body.rating+"') ";
+
+	con.query(query, function (err, result) {
+	     res.redirect(baseURL);
+	});
+});
+
+/* Routing for Edit. This is a GET method to data and prepopulate form*/
+app.get('/event/edit/:id', function(req,res){
+	con.query("SELECT * FROM tracking WHERE id= '"+ req.params.id + "'",
+	function (err,result) {
+		res.render('pages/edit-event',{
+		siteTitle : siteTitle,
+		pageTitle : "Edit event : " + result[0].comments,
+		item: result
+		});
+	});
+});
+
+app.post('/event/edit/:id', function(req,res){
+        var query = "UPDATE `tracking` SET";
+            query += " `comments` =  '"+req.body.comments+"',";
+            query += " `rating` = ' "+req.body.rating+"'";
+	    query += " WHERE `tracking`.`id` =  "+req.body.id+"";
+
+        con.query(query, function (err, result) {
+	  if (result.affectedRows)
+	   {
+             res.redirect(baseURL);
+	   }
+        });
+});
+
+
+
+app.get('/event/delete/:id', function(req,res){
+
+	var query ="DELETE FROM tracking WHERE id ='"+ req.params.id + "'";
+
+	con.query(query, function (err, result) {
+          if (result.affectedRows)
+           {
+             res.redirect(baseURL);
+           }
+	 });
+});
 
 var server = app.listen(80, function(){
 	console.log("Server started port 80");
